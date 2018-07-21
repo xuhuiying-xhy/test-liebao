24d373a24eeb9df61636957f52a938f0935255ec
xuhuiying@cmcm.com <xuhuiying@cmcm.com>
2018-07-21 20:58:21 +0800

    test

---
+++ SRWebSocketTest/SRWebSocketTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SRWebSocketTest
+//
+//  Created by xuhuiying on 2018/7/17.
+//  Copyright © 2018年 xuhuiying. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
